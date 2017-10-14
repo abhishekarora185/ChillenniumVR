868e6221b89bc74591496d558a7ab8b8f36d4f1d
William Bracho Blok <williambrachoblok@gmail.com>
2017-10-14 09:07:27 -0500

    Script changes

---
+++ ChillVR/Assets/Scripts/LampShadeBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampShadeBehavior : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+        transform.parent = null;
+    }
+}
